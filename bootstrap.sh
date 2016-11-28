fa98c2fd3d2713a0946d7d4854133724a33c72b7
Zenified <zenified@riseup.net>
2016-11-28 09:52:59 +0000

    Fix few issues

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
 URL="https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.3-5.5-alpha-20161013-linux_x86_64-portable.tar.bz2"
 THENPWD=$(pwd)
 # Fetch PyPy3
 wget -o pypy.tar.bz2 $URL
 sha512sum pypy.tar.bz2 | grep 1cd7a00da376b2db29b3e1f3e9bb7a77afc8ad988b3f13fd0805f37b23960a34
-c = $?
+c=$?
 if [[ $c != 0 ]]; then
   echo "MITM detected, the hash didn't match !!!"
   exit
